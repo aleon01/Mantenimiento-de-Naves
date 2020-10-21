41047e361950beae9dab55a2c773e77667cb64e5
aleon01 <aleon@ucundinamarca.edu.co>
2020-10-21 11:32:56 -0500

    ActualizadoReg

--- app/src/main/java/com/unicundi/mantenimientodenaves/RegistroUser.java
+++ app/src/main/java/com/unicundi/mantenimientodenaves/RegistroUser.java
@@ -1,37 +1,53 @@
 package com.unicundi.mantenimientodenaves;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.unicundi.mantenimientodenaves.model.Usuarios;
-
-import java.util.Objects;
 
 public class RegistroUser extends AppCompatActivity {
 
-    private EditText correoEdit, nombreEdit, apellidoEdit, identifiacionEdit, direccionEdit, telefonoEdit, codigo, contrasenaEdit, confirmarcontrasenaEdit;
-    private FirebaseAuth mAuth;
+    Usuarios user = new Usuarios();
+    private EditText nombreEdit, apellidoEdit, identifiacionEdit, direccionEdit, telefonoEdit, correoEdit, codigo, contrasenaEdit, confirmarcontrasenaEdit;
 
-    private String email = "";
-    private String password = "";
+    final FirebaseAuth _auth = FirebaseAuth.getInstance();
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    Spinner documentoSpinner;
+    String[] documento;
+
+    private String nombre;
+    private String apellido;
+    private String tipoIdentificacion;
+    private String identificacion;
+    private String telefono;
+    private String direccion;
+    private String email;
+    private String password;
+    private String confcontrasena;
+    private int rol;  //rol empleado
+    private int estado;  //estado activo
+
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     Usuarios usuarios = new Usuarios();
 
@@ -40,34 +56,152 @@ public class RegistroUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_user);
 
-        mAuth = FirebaseAuth.getInstance();
-
+        nombreEdit = findViewById(R.id.editTextNombre);
+        apellidoEdit = findViewById(R.id.editTextApellido);
+        documentoSpinner = (Spinner) findViewById(R.id.tipoIdentificacionEdit);
+        documento = getResources().getStringArray(R.array.tipodocumento);
+        identifiacionEdit = findViewById(R.id.editTextIdentificacion);
+        telefonoEdit = findViewById(R.id.editTextTelefono);
+        direccionEdit = findViewById(R.id.editTextDireccion);
         correoEdit = findViewById(R.id.editTextCorreo);
         contrasenaEdit = findViewById(R.id.editTextContrasena);
+        confirmarcontrasenaEdit = findViewById(R.id.editTextConfContrasena);
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, documento);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        documentoSpinner.setAdapter(adapter);
+
+        inicializarFirebase();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
     public void registros(View view){
-        try {
-            String id = Objects.requireNonNull(mAuth.getCurrentUser().getUid());
-            mDatabase.child("usuarios").child(id).addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.exists()){
-                        usuarios.setNombre(snapshot.child("nombre").getValue().toString());
-                        usuarios.setApellido(snapshot.child("nombre").getValue().toString());
-                        usuarios.setIdentifcacion(snapshot.child("nombre").getValue().toString());
+        nombre = nombreEdit.getText().toString();
+        apellido = apellidoEdit.getText().toString();
+        tipoIdentificacion = documentoSpinner.getSelectedItem().toString();
+        identificacion = identifiacionEdit.getText().toString();
+        telefono = telefonoEdit.getText().toString();
+        direccion = direccionEdit.getText().toString();
+        email = correoEdit.getText().toString();
+        password = contrasenaEdit.getText().toString();
+        confcontrasena = confirmarcontrasenaEdit.getText().toString();
+        rol = 3;
+        estado = 1;
+        if(password.equals(confcontrasena)){
+            if (nombre.isEmpty() || apellido.isEmpty() || identificacion.isEmpty() || telefono.isEmpty() || direccion.isEmpty() || email.isEmpty() || password.isEmpty()){
+                validacion();
+            }else {
+                _auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistroUser.this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                       if (task.isSuccessful()) {
+
+                           user.setNombre(nombre);
+                           user.setApellido(apellido);
+                           user.setTidentificacion(tipoIdentificacion);
+                           if (identificacion.length() >= 8 && identificacion.length() <= 10) {
+                               user.setIdentifcacion(identificacion);
+                               if (telefono.length() >= 7 && telefono.length() <= 10){
+                                   user.setTelefono(telefono);
+                                   user.setDireccion(direccion);
+                                   if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                                       user.setCorreo(email);
+                                       if(password.length() >= 6) {
+                                           user.setContrasena(password);
+                                           user.setRol(rol);
+                                           user.setEstado(estado);
+                                           databaseReference.child("usuarios").child(user.getNombre()).setValue(user);
+                                           /**AlertDialog.Builder alerta = new AlertDialog.Builder(RegistroUser.this);
+                                           alerta.setMessage("Registro Exitoso").setCancelable(true).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                               @Override
+                                               public void onClick(DialogInterface dialog, int which) {
+                                                   Intent menu =new Intent(RegistroUser.this, MainActivity.class);
+                                                   startActivity(menu);
+                                               }
+                                           });*/
+                                           Toast.makeText(getApplicationContext(), "Usuario creado." + email, Toast.LENGTH_SHORT).show();
+                                       }else{
+                                           contrasenaEdit.setError("Ingrese min 6 caracteres");
+                                           contrasenaEdit.requestFocus();
+                                       }
+                                   }else{
+                                       correoEdit.setError("Verifique el correo");
+                                       correoEdit.requestFocus();
+                                   }
+                               }else{
+                                   telefonoEdit.setError("Verifique el telefono");
+                                   telefonoEdit.requestFocus();
+                               }
+                           }else{
+                               identifiacionEdit.setError("Verifique la Identificación");
+                               identifiacionEdit.requestFocus();
+                           }
+                           limpiarCajas();
+
+                       } else {
+                          Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                       }
                     }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
-                }
-            });
-        }catch (Exception e){
-
+                });
+            }
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void limpiarCajas(){
+        nombreEdit.setText("");
+        apellidoEdit.setText("");
+        identifiacionEdit.setText("");
+        telefonoEdit.setText("");
+        direccionEdit.setText("");
+        correoEdit.setText("");
+        contrasenaEdit.setText("");
+        confirmarcontrasenaEdit.setText("");
+    }
+
+    private void validacion(){
+        String nombre = nombreEdit.getText().toString();
+        String apellido = apellidoEdit.getText().toString();
+        String identificacion = identifiacionEdit.getText().toString();
+        String telefono = telefonoEdit.getText().toString();
+        String direccion = direccionEdit.getText().toString();
+        String correo = correoEdit.getText().toString();
+        String contrasena = contrasenaEdit.getText().toString();
+
+        if(nombre.equals("")){
+            nombreEdit.setError("Requerido");
+        } else if(apellido.equals("")){
+            apellidoEdit.setError("Requerido");
+        } else if(identificacion.equals("")){
+            identifiacionEdit.setError("Requerido");
+        } else if(telefono.equals("")){
+            telefonoEdit.setError("Requerido");
+        } else if(direccion.equals("")){
+            direccionEdit.setError("Requerido");
+        } else if(correo.equals("")){
+            correoEdit.setError("Requerido");
+        } else if(contrasena.equals("")){
+            contrasenaEdit.setError("Requerido");
+        }
+    }
+
+    /**public void tDocumento(){
+        documentoSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                user.setTidentificacion(documento[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(RegistroUser.this, "Seleccione un item", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }*/
 }
